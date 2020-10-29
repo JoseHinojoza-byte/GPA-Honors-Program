e447b692824c4902083073c94fdf97d3acd5b845
Jose Hinojoza <jhinojoza@my.dom.edu>
2020-10-29 18:14:54 -0500

    Importing Project

---
+++ A1_Hinojoza/Assignment.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace A1_Hinojoza
+{
+    partial class Assignment
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtGPA = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnHonors = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(145, 69);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(100, 20);
+            this.txtFirstName.TabIndex = 0;
+            // 
+            // txtGPA
+            // 
+            this.txtGPA.Location = new System.Drawing.Point(145, 116);
+            this.txtGPA.Name = "txtGPA";
+            this.txtGPA.Size = new System.Drawing.Size(100, 20);
+            this.txtGPA.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(79, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "First Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(107, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "GPA:";
+            // 
+            // btnHonors
+            // 
+            this.btnHonors.Location = new System.Drawing.Point(62, 182);
+            this.btnHonors.Name = "btnHonors";
+            this.btnHonors.Size = new System.Drawing.Size(126, 23);
+            this.btnHonors.TabIndex = 4;
+            this.btnHonors.Text = "Honors Student?";
+            this.btnHonors.UseVisualStyleBackColor = true;
+            this.btnHonors.Click += new System.EventHandler(this.btnHonors_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(212, 182);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Location = new System.Drawing.Point(82, 151);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(0, 13);
+            this.lblMessage.TabIndex = 6;
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Location = new System.Drawing.Point(308, 30);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(0, 13);
+            this.lblDate.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 297);
+            this.Controls.Add(this.lblDate);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnHonors);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtGPA);
+            this.Controls.Add(this.txtFirstName);
+            this.Name = "Form1";
+            this.Text = "Assignment1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtGPA;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnHonors;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.Label lblDate;
+    }
+}
+
---
+++ A1_Hinojoza/Assignment.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace A1_Hinojoza
+{
+    public partial class Assignment : Form
+    {
+        //program sees if student is elible for honors
+        //Stores user name and tells them their eligibility
+        public Assignment()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblDate.Text = DateTime.Now.ToString("MMM dd, yyyy");
+        }
+        private void btnHonors_Click(object sender, EventArgs e)
+        {
+            double gpa;
+
+            try
+            {
+                gpa = Convert.ToSingle(txtGPA.Text);
+                lblMessage.Text = "";
+
+                if (gpa <= 0 || gpa > 4)
+                {
+                    MessageBox.Show("Please enter a valid GPA", "Invalid GPA",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else if (gpa >= 3.75)
+                {
+                    lblMessage.Text = "HONORS STUDENT";
+                    MessageBox.Show("Congratulations " + txtFirstName.Text.ToUpper
+                    () +
+                                            "!! You will graduate with honors.",
+                    "HONORS", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+                else // No Honors
+                {
+                    MessageBox.Show("Sorry ­ No honors for you ­ better luck next time", "No Honors");
+                }
+
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
